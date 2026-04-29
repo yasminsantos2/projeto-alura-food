d5a627f5e5675cd50741dc9efb91755d6a0e326b
yasminsantos2 <yasminsantos.eng@gmail.com>
2026-04-29 13:52:49 -0300

    feat: configura Feign Client e Eureka Client no microserviço de pagamentos

--- pagamento/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ pagamento/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,10 +2,10 @@ package br.com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableFeignClients
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
